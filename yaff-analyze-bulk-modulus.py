366e957cc8ef9b9d15b59b39eab1fcd50aa5fd68
Paul Boone <narcissus.pong@gmail.com>
2019-12-01 15:50:25 -0500

    make runnable wihtout yaff dependencies

--- yaff-analyze-bulk-modulus.py
+++ yaff-analyze-bulk-modulus.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
-
-
-import matplotlib.pyplot as pt
+import matplotlib.pyplot as plt
 
 import numpy as np
 import h5py as h5
 
-from yaff import angstrom, pascal, electronvolt, boltzmann
+meter = 1.0/0.5291772083e-10
+angstrom = 1.0e-10*meter
+joule = 1/4.35974381e-18
+newton = joule/meter
+pascal = newton/meter**2
+second = 1/2.418884326500e-17
+coulomb = 1.0/1.602176462e-19
+ampere = coulomb/second
+electronvolt = (1.0/coulomb)*joule
+boltzmann = 3.1668154051341965e-06
 
 # Just to give you an idea, kT at room temperature in electronvolt.
 print('kT [eV]', boltzmann*300/electronvolt)
@@ -28,51 +34,35 @@ e *= e_unit
 
 # Select the reference volume.
 v_ref = v[abs(p).argmin()]
-
-# Compute the dimensionless volume.
 x = v/v_ref
 
 # Define an auxiliary array of dimensionless volumes to facilite the plots of
 # the models
 x_aux = np.linspace(x.min(), x.max(), 100)
 
+# fit p(v), dm=design matrix, ev=expected values
+dm = np.array([x, np.ones(len(v))]).T
+ev = p
+a, b = np.linalg.lstsq(dm, ev)[0]
+print('    Bulk modulus [MPa]', -a/p_unit)
 
-def fit_linear_pv():
-    print('Fit linear model to P(V/V_0)')
+fs = 12
+fig = plt.figure(figsize=(2.5,2.0))
+fig.set_tight_layout(True)
+# fig.subplots_adjust(left=0.4,right=0.95, top=0.95, bottom=0.20)
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlim(0.85, 1.1)
+ax.set_ylim(-1.2, 2.2)
+# ax.set_xlabel('V/V_0', fontsize=fs)
+# ax.set_ylabel('Pressure [MPa]', fontsize=fs)
+ax.grid(linestyle='-', color='0.7', zorder=0)
 
-    # fit p(v), dm=design matrix, ev=expected values
-    dm = np.array([x, np.ones(len(v))]).T
-    ev = p
-    a, b = np.linalg.lstsq(dm, ev)[0]
-    print('    Bulk modulus [MPa]', -a/p_unit)
-
-    # plot
-    pt.clf()
-    pt.plot(x, p/p_unit, 'ko')
-    pt.plot(x_aux, (a*x_aux + b)/p_unit, 'r-')
-    pt.xlabel('V/V_0')
-    pt.ylabel('Pressure [MPa]')
-    pt.savefig('pv.png')
-
-
-def fit_quadratic_ev():
-    print('Fit quadratic model to E(V/V_0)')
-
-    # fit e(v), dm=design matrix, ev=expected values
-    dm = np.array([0.5*x**2, x, np.ones(len(v))]).T
-    ev = e
-    a, b, c = np.linalg.lstsq(dm, ev)[0]
-    print('    Bulk modulus [MPa]', a/v_ref/p_unit)
-    print('    Energy [eV]', (c - a**2/2/b)/e_unit)
-
-    # plot
-    pt.clf()
-    pt.plot(x, e/e_unit, 'ko')
-    pt.plot(x_aux, (0.5*a*x_aux**2 + b*x_aux + c)/e_unit, 'r-')
-    pt.xlabel('V/V_0')
-    pt.ylabel('Energy [eV]')
-    pt.savefig('ev.png')
+ax.tick_params(axis='x', which='major', labelsize=fs)
+ax.tick_params(axis='y', which='major', labelsize=fs)
 
 
-fit_linear_pv()
-fit_quadratic_ev()
+plt.plot(x, p/(p_unit * 1000), 'ko', markersize=3)
+
+plt.plot(x_aux, (a*x_aux + b)/(p_unit * 1000), 'r-', label="%3.1f" % (a/(p_unit * 1000)))
+ax.legend(framealpha=1.0, fontsize=fs-2)
+fig.savefig("pv.png", dpi=300)
